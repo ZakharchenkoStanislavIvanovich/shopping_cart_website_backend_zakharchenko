b36cf18d83bb0f34d8d200f5c71dcebcfda7e88c
ZakharchenkoStanislavIvanovich <staszaharchenko.sz@gmail.com>
2025-11-25 20:11:10 +0200

    Added Prisma, fixed db configuration

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useLogger(app.get(Logger))
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true}));
+  app.useLogger(app.get(Logger));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
 bootstrap();
