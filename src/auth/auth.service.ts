28c3aba5882db2403093ac8542d19662c922ba8c
ZakharchenkoStanislavIvanovich <staszaharchenko.sz@gmail.com>
2025-11-27 23:06:25 +0200

    Added Sign JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,41 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import ms from 'ms';
 import * as bcrypt from 'bcrypt';
-import { UsersService } from 'src/users/users.service';
+import { User } from 'prisma/generated';
+import { Response } from 'express';
+import { UsersService } from '../users/users.service';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from './token-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly configService: ConfigService,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async login(user: User, response: Response) {
+        const expires = new Date();
+        expires.setMilliseconds(
+            expires.getMilliseconds() + 
+            ms(this.configService.getOrThrow<string>('JWT_EXPIRATION') as unknown as ms.StringValue),
+        );
+
+        const TokenPayload: TokenPayload = {
+            userId: user.id,
+        };
+        const token = this.jwtService.sign(TokenPayload);
+
+        response.cookie('Authentication', token, {
+            secure: true,
+            httpOnly: true,
+            expires
+        });
+
+        return { TokenPayload };
+    }
 
     async verifyUser(email: string, password: string) {
         try {
