db044867da212afbcedec6ac6bc74540dfd8d129
ZakharchenkoStanislavIvanovich <staszaharchenko.sz@gmail.com>
2025-12-06 22:28:33 +0200

    Added single product routing & backend

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,56 +1,75 @@
-import { Body, Controller, FileTypeValidator, Get, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreateProductRequest } from './dto/create-product.request';
-import { CurrentUser } from 'src/auth/current-user.decorator';
-import { TokenPayload } from 'src/auth/token-payload.interface';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { TokenPayload } from '../auth/token-payload.interface';
 import { ProductsService } from './products.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
+import { PRODUCT_IMAGES } from './product-images';
 
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) {}
 
-    @Post()
-    @UseGuards(JwtAuthGuard)
-    async createProduct(
-        @Body() body: CreateProductRequest,
-        @CurrentUser() user: TokenPayload
-    ) {
-        return this.productService.createProduct(body, user.userId);
-    }
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createProduct(
+    @Body() body: CreateProductRequest,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    return this.productsService.createProduct(body, user.userId);
+  }
 
-    @Post(':productId/image')
-    @UseGuards(JwtAuthGuard)
-    @UseInterceptors(
-        FileInterceptor('image', {
-            storage: diskStorage({
-                destination: 'public/products',
-                filename: (req, file, callback) => {
-                    callback(
-                        null,
-                        `${req.params.productId}${extname(file.originalname)}`,
-                    );
-                },
-            }),
-        }),
+  @Post(':productId/image')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: PRODUCT_IMAGES,
+        filename: (req, file, callback) => {
+          callback(
+            null,
+            `${req.params.productId}${extname(file.originalname)}`,
+          );
+        },
+      }),
+    }),
+  )
+  uploadProductImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 500000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
     )
-    uploadProductImage(
-        @UploadedFile(
-            new ParseFilePipe({
-                validators: [
-                    new MaxFileSizeValidator({ maxSize: 500000 }),
-                    new FileTypeValidator({ fileType: 'image/jpeg' }),
-                ],
-            }),
-        )
-        __file: Express.Multer.File,
-    ) {}
+    _file: Express.Multer.File,
+  ) {}
 
-    @Get()
-    @UseGuards(JwtAuthGuard)
-    async getProducts() {
-        return this.productService.getProducts();
-    }
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getProducts() {
+    return this.productsService.getProducts();
+  }
+
+  @Get(':productId')
+  @UseGuards(JwtAuthGuard)
+  async getProduct(@Param('productId') productId: string) {
+    return this.productsService.getProduct(+productId);
+  }
 }
