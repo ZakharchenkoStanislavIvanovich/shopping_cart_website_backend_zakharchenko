1ad0a4cfba2b9dcbb4d1b854446374ac4c31c79f
ZakharchenkoStanislavIvanovich <staszaharchenko.sz@gmail.com>
2025-12-07 14:02:43 +0200

    Fixed checkout.service.ts

--- src/checkout/checkout.service.ts
+++ src/checkout/checkout.service.ts
@@ -5,31 +5,33 @@ import Stripe from 'stripe';
 
 @Injectable()
 export class CheckoutService {
-    constructor(
-        private readonly stripe: Stripe,
-        private readonly productService: ProductsService,
-        private readonly configService: ConfigService,
-    ) {}
+  constructor(
+    private readonly stripe: Stripe,
+    private readonly productService: ProductsService,
+    private readonly configService: ConfigService,
+  ) {}
 
-    async createSession(productId: number) {
-        const product = await this.productService.getProduct(productId);
-        return this.stripe.checkout.sessions.create({
-            line_items: [
-                {
-                    price_data: {
-                        currency: 'usd',
-                        unit_amount: product.price * 100,
-                        product_data: {
-                            name: product.name,
-                            description: product.description,
-                        },
-                    },
-                    quantity: 1,
-                },
-            ],
-            mode: 'payment',
-            success_url: this.configService.getOrThrow('STRIPE_SUCCESS_URL'),
-            cancel_url: this.configService.getOrThrow('STRIPE_CANCEL_URL'),
-        });
-    }
+  async createSession(productId: number) {
+    const product = await this.productService.getProduct(productId);
+
+    const session = await this.stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            unit_amount: product.price * 100,
+            product_data: {
+              name: product.name,
+              description: product.description,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: this.configService.getOrThrow('STRIPE_SUCCESS_URL'),
+      cancel_url: this.configService.getOrThrow('STRIPE_CANCEL_URL'),
+    });
+    return { url: session.url };
+  }
 }
